ef8fa302397c92563d29fdd571930979dcf7820d
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2023-01-18 15:31:21 +0530

    blackJack

---
+++ index.js
@@ -0,0 +1,67 @@
+// let firstCard = 12
+// let secondCard = 9
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ''
+let messageEl = document.getElementById('message-el')
+let cardsEl = document.getElementById('cards-el')
+let sumEl = document.getElementById('sum-el')
+let playerEl = document.getElementById('player-el')
+
+let player = {
+    name: 'Rahul',
+    chips: 120
+}
+playerEl.textContent = player.name +": $"+ player.chips
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13+1)
+    if (randomNumber>10){
+        return 10
+    } else if (randomNumber===1){
+        return 11
+    } else {
+        return randomNumber
+    }
+};
+
+function startGame(){
+    isAlive  =true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    rendorGame()
+}
+
+function rendorGame(){
+    sumEl.textContent = `Sum: ${sum}`
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i<cards.length; i++){
+        cardsEl.textContent+= cards[i] + " "
+    }
+    if (sum<=20){
+        message=('Do you want to draw a new card? ')
+    } else if(sum===21){
+        message=("You've got a black jack!")
+        hasBlackJack = true
+    } else {
+        message=('You are out of the game.')
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    if (isAlive===true && hasBlackJack===false){
+        let card = getRandomCard()
+    sum+= card
+    cards.push(card)
+    rendorGame()
+    }
+}
+
+console.log(message)
+console.log(isAlive)
